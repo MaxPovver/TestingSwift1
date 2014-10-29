80d504c65d0682763efc7b993628e5a0c6351aa2
MaxPovver <chistov_maxim@mail.ru>
2014-10-29 13:09:21 +0300

    ...

--- TestingSwift1/ViewController.swift
+++ TestingSwift1/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController /*, UITableViewDataSource, UITableViewDelegate*/ {
 //@IBOutlet weak var table: UITableView!
+let apiURL="https://worlteam-m.com/mobile/api.php?q=" //адрес для запросов к "апи", к нему надо просто прибавить JSON в виде строки
+let registerTemplate="{\"action\":\"register\",\"phone\":\"79150000000\"}"//
+let loginTemplate="{\"action\":\"login\",\"username\":\"79153832915\",\"password\":\"12345\"}"//
+let loginResponseTemplate="{\"code\":\"ok\",\"token\":\"<token-val>\"}"
+let logoutTemplate="{\"action\":\"logout\",\"token\":\"<token-val>\"}"
+var phone = ""
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,8 +25,22 @@ class ViewController: UIViewController /*, UITableViewDataSource, UITableViewDel
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    @IBAction func phoneEdited(sender :UITextField)
+    {
+        phone = sender.text //при каждом редактировании сохараняем в нашу переменную новое значение поля
+    }
     
-    @IBAction func register(sender :UIButton)//это делается при нажатии кнопки регистрацции.
+   @IBAction func register(sender :UIButton)//это делается при нажатии кнопки регистрацции.
+    {
+         var phoneNumber = phone //вытаскиваем сюда значение телефона, который чувак зарегать хочет
+        if phoneNumber.utf16Count > 5 {//если номер нормальной длины
+            var result = tryRegister()
+            if result.code != "OK" {
+                
+            }
+        }
+    }
+    func tryRegister(phone :)
     {
         
     }
